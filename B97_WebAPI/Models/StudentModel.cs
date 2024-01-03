eb4a4670c80c44e50e423468a4c55b882b372002
Dinesh Kumar <dineshraz1234@gmail.com>
2024-01-03 20:08:10 +0530

    Added a Comment to Student Model

--- B97_WebAPI/Models/StudentModel.cs
+++ B97_WebAPI/Models/StudentModel.cs
@@ -3,6 +3,7 @@
     public class StudentModel
     {
         public int StudentId { get; set; }
+        //This is a COMMENT for GIT
         public string? StudentName { get; set; }
         public DateTime? StudentDateofBirth { get; set; }
         public string? BranchName { get; set; }
