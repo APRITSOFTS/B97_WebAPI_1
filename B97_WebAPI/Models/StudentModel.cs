4f29fd43e638089e61462aea74b4b1b7349be6be
Abhishek <abhishek@ba365.com.au>
2024-01-03 20:11:47 +0530

    Added a comment and a property to the sstudent model

--- B97_WebAPI/Models/StudentModel.cs
+++ B97_WebAPI/Models/StudentModel.cs
@@ -4,9 +4,12 @@
     {
         public int StudentId { get; set; }
         //This is a COMMENT for GIT
+        //This is a Comment by Abhishek or Satya
         public string? StudentName { get; set; }
         public DateTime? StudentDateofBirth { get; set; }
         public string? BranchName { get; set; }
         public string? Gender { get; set; }
+
+        public string? FatherName { get; set; } 
     }
 }
