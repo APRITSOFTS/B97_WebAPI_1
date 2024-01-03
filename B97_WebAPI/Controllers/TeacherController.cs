257b27c6e56997a53da6c0aa39edd27b686ed93f
Abhishek <abhishek@ba365.com.au>
2024-01-03 20:28:06 +0530

    Added GetAllTeacher Functionality

---
+++ B97_WebAPI/Controllers/TeacherController.cs
@@ -0,0 +1,33 @@
+﻿using B97_WebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B97_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        [HttpGet]
+        public List<TeacherModel> GetAllTeachers()
+        {
+            return GetTeachers();
+        }
+
+        private List<TeacherModel> GetTeachers()
+        {
+            List<TeacherModel> teacherModels = new List<TeacherModel>();
+            teacherModels.Add(new TeacherModel
+            {
+                TeacherId = 2001,
+                TeacherName = "Keerthi"
+            });
+            teacherModels.Add(new TeacherModel
+            {
+                TeacherId = 2002,
+                TeacherName = "Shankar"
+            });
+            return teacherModels;
+        }
+    }
+}
---
+++ B97_WebAPI/Models/TeacherModel.cs
@@ -0,0 +1,8 @@
+﻿namespace B97_WebAPI.Models
+{
+    public class TeacherModel
+    {
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+    }
+}
